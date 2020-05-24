889938d82d1cd22796d44de862be5a3fac0faa48
Christopher Deane <chris@astrodyne.co.uk>
2020-05-24 13:24:12 +0100

    Added menu and improved game play

---
+++ FlappyBird/Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuEvents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetGUIVisibile(GameObject go)
+    {
+        go.SetActive(true);
+    }
+
+    public void SetGUIInvisibile(GameObject go)
+    {
+        go.SetActive(false);
+    }
+
+    public void OpenMenu(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+
+    public void CloseMenu(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
+    public void togglePause(bool pause)
+    {
+        if (pause)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+}
--- FlappyBird/Assets/Scripts/PlayerController.cs
+++ FlappyBird/Assets/Scripts/PlayerController.cs
@@ -6,20 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     public float MaxTimeTillJump = 1.5f;
     private float timeTillCanJump = 0f;
+    private bool gameRunning = true;
 
     Rigidbody rig;
+    MenuEvents menuEventsScript;
+    public GameObject endGamePanel;
+    public GameObject continueButton;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (endGamePanel == null) Debug.Log("Failed to load menu!");
+        menuEventsScript = GameObject.Find("Canvas").GetComponent<MenuEvents>();
         rig = gameObject.GetComponent<Rigidbody>();
         timeTillCanJump = MaxTimeTillJump;//Allow to jump from the very start
+        menuEventsScript.togglePause(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) && timeTillCanJump >= MaxTimeTillJump)
+        if (Input.GetKeyDown(KeyCode.W) && gameRunning && timeTillCanJump >= MaxTimeTillJump)
         {
             rig.velocity = new Vector3(rig.velocity.x, 10f, rig.velocity.z);
             timeTillCanJump = 0f;
@@ -32,15 +39,23 @@ public class PlayerController : MonoBehaviour
     public void hitObstacle(string obstacleTag)
     {
         MaxTimeTillJump = 1000000f;
+        gameRunning = false;
         Debug.Log("Hit player");
     }
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.tag == "Obstacle")
+        if (collider.gameObject.tag == "Obstacle" || collider.gameObject.tag == "Floor")
         {
             Debug.Log("Player Collider");
             hitObstacle(gameObject.tag);
+        }
+
+        if (collider.gameObject.tag == "Floor")
+        {
+            menuEventsScript.togglePause(true);
+            menuEventsScript.SetGUIInvisibile(continueButton);
+            menuEventsScript.OpenMenu(endGamePanel);
         }
     }
 
